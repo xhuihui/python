b7c00ea64415a0ff72fc4460eb6b910a3ab08b8b
liqinghui <liqinghuiyx@126.com>
2018-01-30 00:17:13 +0800

    Update pv.py

--- pv.py
+++ pv.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests
 for x in range(100000):
   requests.get('https://fp.corpautohome.com/data/ap2018/pic/wx/list/7viYz2?callback=jsonp1&_=1517242304898')
